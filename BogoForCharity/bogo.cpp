c4f4fb37746791f5441cb22998d9e5737d87a949
STGDanny <ste4lthytaco@gmail.com>
2019-10-08 16:48:40 -0400

    Updated comments. Abandoned gtk to move to a simple ASCII GUI. Project should be ready for initial test. Removed unused imports.

--- BogoForCharity/bogo.cpp
+++ BogoForCharity/bogo.cpp
@@ -8,8 +8,9 @@
 */
 
 #include <stdio.h>
-#include <random>
 #include <time.h>
+#include <chrono>
+#include <thread>
 
 //Function prototypes
 void fillArray(int*, int);
@@ -17,11 +18,14 @@ void bogoSort(int[], int);
 int checkIfSorted(int[], int);
 void swap(int*, int*);
 void shuffle(int[], int);
+void printArr(int arr[], int size, int a, int b);
+void printArr(int arr[], int size);
 
 //Driver code for bogo
 int main() {
 	//Seed random number generator using current time (very random value)
 	srand(time(NULL));
+
 	int calculatedTime = 0;
 
 	//Loop will run bogo, increase the size of the array each loop
@@ -37,6 +41,9 @@ int main() {
 		//Shuffle array before sorting
 		shuffle(arr, sizeOfArray);
 
+		//Print output
+		//printArr(arr, sizeOfArray);
+
 		//Start clock and bogo
 		clock_t begin = clock();
 		bogoSort(arr, sizeOfArray);
@@ -45,11 +52,6 @@ int main() {
 		//Figure out how long that took
 		calculatedTime = (end - begin) / CLOCKS_PER_SEC;
 
-		//Print results
-		for (int i = 0; i < sizeOfArray; i++) {
-			printf("%d\t", arr[i]);
-		}
-		printf("\nSorted %d elements in %d seconds\n", sizeOfArray, calculatedTime);
 		//Free up memory allocated to arr
 		free(arr);
 	}
@@ -58,7 +60,7 @@ int main() {
 
 /*
 *	FUNCTION		: fillArray
-*	DESCRIPTION		: Fills each element of an array with pseudo-random values
+*	DESCRIPTION		: Fills each element of an array with values from 1 to the size of arr
 *
 *	PARAMETERS		:
 *	int* arr 	    : Memory address of an array
@@ -152,9 +154,77 @@ void swap(int* xp, int* yp) {
 *	void			: Void
 */
 void shuffle(int arr[], int size) {
+	using namespace std::chrono_literals;
 	//For every element in arr[], starting from the last element
 	for (int i = 0; i < size; i++) {
 		//Swap current element with a random element in the array
 		swap(&arr[i], &arr[rand() % size]);
 	}
+	printArr(arr, size);
+	std::this_thread::sleep_for(300ms);
+	system("cls");
+}
+
+void printArr(int arr[], int size) {
+	printArr(arr, size, -1, -1);
+}
+
+void printArr(int arr[], int size, int a, int b) {
+	const char CHAR_DEFAULT_BAR = 219;
+	const char CHAR_SWAP_BAR = 219;
+
+	const char CHAR_LEFT1 = 192;
+	const char CHAR_LEFT2 = 196;
+	const char CHAR_RIGHT1 = 217;
+	const char CHAR_RIGHT2 = 196;
+	const char CHAR_MID = 196;
+
+	for (int i = size - 1; i >= 0; i--) {
+		for (int j = 0; j < size; j++) {
+			if (arr[j] > i) {
+				if (j == a || j == b) {
+					printf("%c%c  ", CHAR_SWAP_BAR, CHAR_SWAP_BAR);
+				}
+				else {
+					printf("%c%c  ", CHAR_DEFAULT_BAR, CHAR_DEFAULT_BAR);
+				}
+			}
+			else {
+				printf("    ");
+			}
+		}
+		printf("\n");
+	}
+
+	if (a >= 0 && b >= 0 && a != b) {
+		if (b > a) {
+			swap(&a, &b);
+		}
+
+		for (int i = 0; i < a + 1; i++) {
+			if (i == b) {
+				printf("%c%c%c%c", CHAR_LEFT1, CHAR_LEFT2, CHAR_MID, CHAR_MID);
+			}
+			else if (i == a) {
+				printf("%c%c  ", CHAR_RIGHT2, CHAR_RIGHT1);
+			}
+			else if (i > b&& i < a) {
+				printf("%c%c%c%c", CHAR_MID, CHAR_MID, CHAR_MID, CHAR_MID);
+			}
+			else {
+				printf("    ");
+			}
+		}
+
+		printf("\n");
+	}
+	else {
+		printf("\n");
+	}
+
+	for (int i = 0; i < size; i++) {
+		printf("%02d  ", arr[i]);
+	}
+
+	printf("\n");
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BogoForCharity
 
 # Credits
-Aidan Eastcott for helping with the shuffle algorithm.<br/>
+Aidan Eastcott for helping with the shuffle algorithm and creating the GUI.<br/>
 Scott Page for letting me hijack his Discord calls.
