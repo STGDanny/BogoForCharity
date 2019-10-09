1c79a0890ad8d7af3c940bed4257e850b0ecf58a
STGDanny <ste4lthytaco@gmail.com>
2019-10-08 20:03:41 -0400

    Added file header and comments to proto.h.

--- BogoForCharity/proto.h
+++ BogoForCharity/proto.h
@@ -1,11 +1,22 @@
+/*
+*	FILE			: proto.h
+*	PROJECT			: BogoForCharity
+*	PROGRAMMER(S)	: Daniel Pieczewski, Aidan Eastcott
+*	FIRST VERSION	: 2019-10-8
+*	DESCRIPTION		:
+*		This header is used to link all functions in the project together
+*/
+
 #pragma once
 
-//Function prototypes
+//Function prototypes for bogo.cpp
 void fillArray(int*, int);
 void bogoSort(int[], int);
 int checkIfSorted(int[], int);
 void swap(int*, int*);
 void shuffle(int[], int);
+
+//Function prototypes for gui.cpp
 void printArr(int[], int, int, int);
 void printArr(int[], int);
 void printArr(int[], int, bool);
